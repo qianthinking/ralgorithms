7a1463723b3a2ecdfe5a5eed4bbcb381b33e389d
Leon Li <qianthinking@gmail.com>
2012-03-13 19:30:10 +0800

    refactor quicksort for extract median_of_3

--- sorting/quick_sort.rb
+++ sorting/quick_sort.rb
@@ -23,12 +23,7 @@ module Sorting
     end
 
     def self.partition(data, p, r)
-      m = p + (r-p)/2
-      pivot_candidates_hash = {data[p] => p, data[m] => m, data[r] => r}
-      pivot_candidates = pivot_candidates_hash.keys
-      pivot = (pivot_candidates - pivot_candidates.minmax)[0] || pivot_candidates.last
-      pivot_index = pivot_candidates_hash[pivot]
-      data[pivot_index], data[r] = data[r], pivot if pivot_index != r
+      median_of_3 data, p, r
       pivot = data[r]
       i = p - 1
       p.upto(r-1) do |j|
@@ -39,6 +34,15 @@ module Sorting
       end
       data[i+1],data[r] = data[r],data[i+1]
       return i + 1
+    end
+
+    def self.median_of_3(data, p, r)
+      m = p + (r-p)/2
+      pivot_candidates_hash = {data[p] => p, data[m] => m, data[r] => r}
+      pivot_candidates = pivot_candidates_hash.keys
+      pivot = (pivot_candidates - pivot_candidates.minmax)[0] || pivot_candidates.last
+      pivot_index = pivot_candidates_hash[pivot]
+      data[pivot_index], data[r] = data[r], pivot if pivot_index != r
     end
 
   end
