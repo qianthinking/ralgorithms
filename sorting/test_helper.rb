d5630ee3e4fbf98437dd159e575a4b5a6d8936fd
Leon Li <qianthinking@gmail.com>
2012-03-13 11:28:45 +0800

    improve test to work with sort! and sort

--- sorting/test_helper.rb
+++ sorting/test_helper.rb
@@ -7,18 +7,27 @@ module Sorting
       shuffle ? data.shuffle : data
     end
 
-    def self.test(filename, benchmark_data_size=100000)
+    def self.sort(clazz, data)
+      if clazz.respond_to? :sort!
+        clazz.sort! data
+      else
+        data = clazz.sort data
+      end
+      data
+    end
+
+    def self.test(filename, benchmark_data_size)
       data = get_sample_data
       clazz = eval("Sorting::#{File.basename(filename).split("\.")[0].capitalize}")
       puts "#{clazz} - before sort: #{data.inspect}"
-      clazz.sort! data
+      data = sort(clazz, data)
       puts "#{clazz} - after  sort: #{data.inspect}"
 
       sample_data = get_sample_data benchmark_data_size, false
       benchmark_data = sample_data.dup
       result = Benchmark.measure do
-        clazz.sort! benchmark_data
-        #10.times {clazz.sort! benchmark_data.dup} #notice, you should dup sample data for multiple test
+        benchmark_data = sort(clazz, benchmark_data)
+        #10.times {sort(clazz, benchmark_data.dup)} #notice, you should dup sample data for multiple test
       end
       diff = diff(benchmark_data, sample_data.sort)
       raise "sort wrong for #{sample_data.inspect}, diff: #{diff.inspect}" unless diff.empty?
@@ -28,8 +37,8 @@ module Sorting
       sample_data = get_sample_data benchmark_data_size
       benchmark_data = sample_data.dup
       result = Benchmark.measure do
-        clazz.sort! benchmark_data
-        #10.times {clazz.sort! benchmark_data.dup} #notice, you should dup sample data for multiple test
+        benchmark_data = sort(clazz, benchmark_data)
+        #10.times {sort(clazz, benchmark_data.dup)} #notice, you should dup sample data for multiple test
       end
       diff = diff(benchmark_data, sample_data.sort)
       raise "sort wrong for #{sample_data.inspect}, diff: #{diff.inspect}" unless diff.empty?
