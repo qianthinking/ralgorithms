72bcc3aebbfff993b40941f81edda1782c8f8b43
Leon Li <qianthinking@gmail.com>
2012-03-14 20:53:46 +0800

    refactory insertion sort

--- sorting/insertion_sort.rb
+++ sorting/insertion_sort.rb
@@ -3,45 +3,54 @@ module Sorting
   class InsertionSort
     extend Helper
     TEST_DATA_SIZE=1000
-    BINARY_SEARCH_THRESHOLD=100
+    BINARY_SEARCH_THRESHOLD=80
 
     def self.sort!(data, l=0, r=data.size-1)
       ((l+1)..r).each do |i|
         value = data[i]
-        low = l
-        high = i-1
-        found = nil
-        while low <= high
-          mid = low +  (high-low)/2
-          if data[mid] < value
-            low = mid + 1
-          else
-            found = mid
-            high = mid - 1
-          end
+        j = i
+        while j > l && value < (previous = data[j-1])
+          data[j] = previous
+          j -= 1
         end
-        if found
-          length = i-found
-          data[found+1, length] = data[found,length]
-          data[found] = value
+        data[j] = value
+      end
+      nil
+    end
+
+    def self.binary_sort!(data, l=0, r=data.size-1)
+      ((l+1)..r).each do |i|
+        value = data[i]
+        if (distance = i - l) > BINARY_SEARCH_THRESHOLD
+          low = l
+          high = i-1
+          found = nil
+          while low <= high
+            mid = low +  (high-low)/2
+            if data[mid] < value
+              low = mid + 1
+            else
+              found = mid
+              high = mid - 1
+            end
+          end
+          if found
+            length = i-found
+            data[found+1, length] = data[found,length]
+            data[found] = value
+          end
+        else
+          j = i
+          while j > l && value < (previous = data[j-1])
+            data[j] = previous
+            j -= 1
+          end
+          data[j] = value
         end
       end
       nil
     end
 
-    ##simple version
-    #def self.sort!(data, l=0, r=data.size-1)
-    #  ((l+1)..r).each do |i|
-    #    value = data[i]
-    #    j = i
-    #    while j > l && value < (previous = data[j-1])
-    #      data[j] = previous
-    #      j -= 1
-    #    end
-    #    data[j] = value
-    #  end
-    #  nil
-    #end
 
   end
 end
