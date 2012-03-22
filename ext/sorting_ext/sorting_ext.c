a851e9c09818159028e5b0168e0a2b7ae9a78baa
Leon Li <qianthinking@gmail.com>
2012-03-22 18:31:12 +0800

    add binary insert sort

--- ext/sorting_ext/sorting_ext.c
+++ ext/sorting_ext/sorting_ext.c
@@ -1,26 +1,27 @@
 #include "ruby/ruby.h"
 
+#define BINARY_SEARCH_THRESHOLD 300
+#define BINARY_SEARCH_TEST_DISTANCE 30
+
 VALUE sortingModule;
 VALUE sortingClass;
 
 void
 insert_sort_impl(VALUE* base, const size_t len, long start, long l, long r, 
-	   int (*cmp)(const void*, const void*, void*), void *d)
+        int (*cmp)(const void*, const void*, void*), void *d)
 {
     VALUE obj;
     VALUE previous;
-    unsigned int i, j;
+    long i, j;
     if (l == -1) l = 0;
     if (r == -1) r = len - 1;
     if (start == -1) start = l + 1;
     for(i = start; i <=r; i++) {
         obj = base[i];
         j = i;
-        while (j > l)
-        {
+        while (j > l) {
             previous = base[j-1];
-            if((*cmp)(obj, previous, d) < 0)
-            {
+            if((*cmp)(obj, previous, d) < 0) {
                 base[j] = previous;
                 j--;
             } else break;
@@ -31,6 +32,60 @@ insert_sort_impl(VALUE* base, const size_t len, long start, long l, long r,
 }
 
 void
+binary_insert_sort_impl(VALUE* ary, const size_t len, long start, long l, long r, 
+        int (*cmp)(const void*, const void*, void*), void *d)
+{
+    char *base = (char *)ary;
+    size_t size = sizeof(VALUE);
+    VALUE obj;
+    VALUE previous;
+    long i, j, distance, low, high, mid, found, length;
+    char *dest;
+    char *src;
+    if (l == -1) l = 0;
+    if (r == -1) r = len - 1;
+    if (start == -1) start = l + 1;
+    for(i = start; i <=r; i++) {
+        obj = ary[i];
+        distance = i - l;
+        if (distance > BINARY_SEARCH_THRESHOLD
+                && (*cmp)(obj, ary[i-BINARY_SEARCH_TEST_DISTANCE], d) < 0) {
+            low = l;
+            high = i - 1;
+            found = -1;
+            while (low <= high) {
+                mid = low + (high-low)/2;
+                if ((*cmp)(obj, ary[mid], d) > 0) {
+                    low = mid + 1;
+                } else {
+                    found = mid;
+                    high = mid - 1;
+                }
+            }
+            if(found != -1) {
+                length = i - found;
+                //it could be optimized when length small
+                dest = base + (found+1)*size;
+                src = base + found*size;
+                memmove(dest, src, size*length);
+                ary[found] = obj;
+            }
+        } else {
+            j = i;
+            while (j > l) {
+                previous = ary[j-1];
+                if((*cmp)(obj, previous, d) < 0) {
+                    ary[j] = previous;
+                    j--;
+                } else break;
+            }
+            ary[j] = obj;
+        }
+    }
+
+}
+
+    void
 insert_sort_bang(VALUE dummy, VALUE ary)
 {
     dynamic_sort(ary, insert_sort_impl);
